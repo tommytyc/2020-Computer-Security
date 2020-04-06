6b06a08fe4735ca40c6b29fd927abaa39a3d4cf8
tommytyc <tommytyc.cs06@nctu.edu.tw>
2020-04-06 20:56:07 +0800

    finish checksum but haven't checked

--- dnsamp.cpp
+++ dnsamp.cpp
@@ -8,10 +8,6 @@ void SendDnsPacket(){
 
 }
 
-void SendUdpPacket(){
-
-}
-
 void CreateDnsHeader(dh *dns){
     dns->query_id = (unsigned short) htons(getpid());
     dns->addrr = 0;
@@ -26,7 +22,31 @@ void CreateQueryInfo(query *q){
     q->dnsq_type = htons(1);
 }
 
-// Calculate IP and UDP checksum
-unsigned short CheckSum(int bytes, unsigned short *ptr){
-    
+// Calculate UDP checksum
+unsigned short CheckUdpSum(int length, unsigned short *ptr){
+    ih *ipheader = (ih *)ptr;
+    uh *udpheader = (uh *)(ptr + sizeof(ipheader));
+    udpheader->check = 0;
+    unsigned long sum = 0;
+
+    sum = CheckSum(8, (unsigned short *)ipheader->saddr);
+    sum += CheckSum(length, (unsigned short *)udpheader);
+    sum += ntohs(length + IPPROTO_UDP);
+
+    sum = (sum >> 16) + (sum & 0xffff);
+    sum = sum + (sum >> 16);
+
+    return (unsigned short)~sum;
+
+}
+
+unsigned int CheckSum(int size, unsigned short *ptr){
+    unsigned int cksum;
+    for(cksum = 0; size > 1; size -= 2){
+        cksum += *ptr++;
+    }
+    if(size == 1){
+        cksum += *(unsigned short *)ptr;
+    }
+    return cksum;
 }
